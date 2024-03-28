8d3d33e6004aadbff68d5436361f2cf544c42f6b
Edouard Courty <edouard.courty@causeway.com>
2024-03-28 15:51:43 +0100

    fix (fetching) fix copy/paste dumbness

--- src/repository/LeaderboardRepository.js
+++ src/repository/LeaderboardRepository.js
@@ -1,7 +1,6 @@
 import LeaderboardDenormaliser from "../denormaliser/LeaderboardDenormaliser.js";
 import Leaderboard from "../model/Leaderboard.js";
 import HTTPClient from "../HTTPClient.js";
-import WarSummaryDenormaliser from "../denormaliser/WarSummaryDenormaliser.js";
 import HttpNotFoundException from "../exception/HttpNotFoundException.js";
 import HttpErrorException from "../exception/HttpErrorException.js";
 
@@ -14,7 +13,7 @@ export default class LeaderboardRepository {
         try {
             const response = await HTTPClient.getInstance().get('api/LeaderBoard/HotF/v2/Player/' + warId);
 
-            return WarSummaryDenormaliser.denormaliseWarSummary(response.data)
+            return LeaderboardDenormaliser.denormaliseLeaderboard(response.data)
         } catch (error) {
             if (error.response.status === 400 || error.response.status === 404) {
                 throw new HttpNotFoundException(`Leaderboard for war ${warId} not found.`);
